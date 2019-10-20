f16540c007388e14137b3881196dfd5fa9f3c6c2
Timits <will4f21@yahoo.com>
2019-10-20 01:18:58 -0700

    npm install server packgs mongoose express exphbs

---
+++ server.js
@@ -0,0 +1,34 @@
+const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
+const logger = require("morgan");
+
+const express = require("express");
+const app = express();
+
+app.use(logger("dev"));
+app.use(
+    bodyParser.urlencoded({
+        extended: false
+    })
+);
+
+app.use(express.static(process.cwd() + "/public"));
+
+const exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({defaultLayout: "main"})
+);
+
+app.set("view engine", "handlebars");
+
+mongoose.connect("mongodb://localhost/news_scraper_spacejnk");
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function() {
+    console.log("Connected to Mongoose!");
+});
+
+const port = process.env.PORT || 3001;
+app.listen(port, function() {
+    console.log("Listening on PORT " + port)
+});
